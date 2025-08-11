95f86c08a54c73935147a8622eac96598d1027c2
Nicolas Kaiser <90482203+nikolasluis@users.noreply.github.com>
2025-08-11 19:28:33 -0300

    Update codigo.sql

--- db/codigo.sql
+++ db/codigo.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS docker_e_kubernetes;
 
 USE docker_e_kubernetes;
 
-CREATE TABLE atividade02 (
+CREATE TABLE IF NOT EXISTS atividade02 (
     ID INT NOT NULL,
     FirstName VARCHAR(30) NOT NULL,
     LastName VARCHAR(100),
